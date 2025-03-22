3aa22874f844952cb64e3c36ff72f65ed4f75573
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-03-22 00:26:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const spaceViews = [
+    {path: './img/crescent_nebula.webp', alt: 'a crescent nebula appears like a pink and mauve cloud of stars in front of a star-splattered stretch of space.'},
+    {path: './img/orion_nebula.webp', alt: 'the Orion Nebula appears like glowing pink and purple petals with stars scattered all around.'},
+    {path: './img/milky_way.webp', alt: 'the Milky Way galaxy appears like a slightly crooked branch made of stars with countless other stars spread around it.'},
+    {path: './img/cosmic_cloud.webp', alt: 'a cosmic dust cloud appears like a frozen yellow flame inside a disk of orange and brown clouds with pink vapor and stars all around.'}
+];
+const abelPics = [
+    {path: './img/space_abel_0.jpg', alt: 'a young boy with light brown skin and curly hair gazes forward, wearing a red shirt, a microphone headset, and a serious expression.'},
+    {path: './img/space_abel_1.jpg', alt: 'a young boy with light brown skin and curly hair gazes forward, wearing a red shirt, a microphone headset, and a slightly angry expression.'},
+    {path: './img/space_abel_2.jpg', alt: 'a young boy with light brown skin and curly hair holds the microphone on his headset and gazes forward thoughtfully.'},
+    {path: './img/space_abel_3.jpg', alt: 'a young boy with light brown skin and curly hair gazes forward, wearing a red shirt, a microphone headset, and a slight smile.'}
+]
+
+window.onload = ()=> {
+    document.getElementById('main').classList.add('crescent-nebula');
+};
