81e0670f754df082f5d62ba035aa0c40a3d063d4
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-03-24 18:21:08 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
+//All the space pictures are in this array. An array is a collection of things.
 const spaceViews = [
-    {path: './img/crescent_nebula.webp', alt: 'a crescent nebula appears like a pink and mauve cloud of stars in front of a star-splattered stretch of space.'},
-    {path: './img/orion_nebula.webp', alt: 'the Orion Nebula appears like glowing pink and purple petals with stars scattered all around.'},
-    {path: './img/milky_way.webp', alt: 'the Milky Way galaxy appears like a slightly crooked branch made of stars with countless other stars spread around it.'},
-    {path: './img/cosmic_cloud.webp', alt: 'a cosmic dust cloud appears like a frozen yellow flame inside a disk of orange and brown clouds with pink vapor and stars all around.'}
+    {imageName: 'crescent-nebula', alt: 'a crescent nebula appears like a pink and mauve cloud of stars in front of a star-splattered stretch of space.'},
+    {imageName: 'orion-nebula', alt: 'the Orion Nebula appears like glowing pink and purple petals with stars scattered all around.'},
+    {imageName: 'milky-way', alt: 'the Milky Way galaxy appears like a slightly crooked branch made of stars with countless other stars spread around it.'},
+    {imageName: 'cosmic-cloud', alt: 'a cosmic dust cloud appears like a frozen yellow flame inside a disk of orange and brown clouds with pink vapor and stars all around.'}
 ];
+//All the Abel pictures are in this array.
 const abelPics = [
     {path: './img/space_abel_0.jpg', alt: 'a young boy with light brown skin and curly hair gazes forward, wearing a red shirt, a microphone headset, and a serious expression.'},
     {path: './img/space_abel_1.jpg', alt: 'a young boy with light brown skin and curly hair gazes forward, wearing a red shirt, a microphone headset, and a slightly angry expression.'},
     {path: './img/space_abel_2.jpg', alt: 'a young boy with light brown skin and curly hair holds the microphone on his headset and gazes forward thoughtfully.'},
     {path: './img/space_abel_3.jpg', alt: 'a young boy with light brown skin and curly hair gazes forward, wearing a red shirt, a microphone headset, and a slight smile.'}
 ]
-
+//All the facts about space are in this array.
+const spaceFacts = [
+    'Fact one', 'fact two', 'fact three'
+]
+//This function tells the webpage what to do when it first loads
 window.onload = ()=> {
-    document.getElementById('main').classList.add('crescent-nebula');
+    //This part of the function gets a random whole number between 0 and 3 and assigns it to a variable called randomNumber.
+    const randomNumber = Math.floor(Math.random() * spaceViews.length);
+    //Then we take the random number and look to see which picture is in that index in the array. An index is like a place in line.
+    //We use the picture at the random number's index to give the "space" section of the webpage a background.
+    document.getElementById('space').classList.add(spaceViews[randomNumber]['imageName']);
 };
